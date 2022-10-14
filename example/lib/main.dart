9d80e679d74075de1b3f9c3e9d8e4ae9b41f1057
Bartosz Wróbel <bwrobel@everpress.pl>
2022-10-14 03:23:33 +0200

    update
    moveDistance property added
    timerRestproperty added
    defaultTextStyle added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -122,7 +122,7 @@ class _VerticalExampleState extends State<VerticalExample> {
               text:
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
               textStyle: TextStyle(fontSize: 24),
-              scrollAxis: Axis.vertical,
+              scrollDirection: Axis.vertical,
             ),
           ],
         ),
